0965957cfecbaa760316755cae195dca5149213a
Fyodor Sizov <f.sizov@yandex.ru>
2019-04-04 16:58:56 +0300

    Update dmSiteSkin.top.js

--- web_src/dmSiteSkin.top.js
+++ web_src/dmSiteSkin.top.js
@@ -3,8 +3,3 @@ function disripp(el) {
       $(el).find('div').remove();
     } catch (e) {};
 }
-audio = new Audio('sable.mp3');
-$('.navbar-brand').click(function () {
-    audio.play();
-    setTimeout(function(){audio.pause();}, 5000);
-});
